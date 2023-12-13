d2d39e770b90fb639a5ed05a31479cd5e1418b29
Michele <michele.pagot.dev@outlook.com>
2023-12-13 22:43:17 +0000

    2023 day5 part1
    but it does not scale for part2 :-(

--- 2023/5/main.py
+++ 2023/5/main.py
@@ -6,18 +6,128 @@ import logging
 import re
 
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 handler = logging.StreamHandler()
-formatter = logging.Formatter(fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-handler.setFormatter(formatter)
+handler.setFormatter(
+    logging.Formatter(fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    )
 log.addHandler(handler)
+
+
+def decode_almanac(lines):
+    """
+    From input text to seed list and almanac dict
+    """
+    almanac = {}
+    seeds = None
+    # regexp to read the first line of the file, the list of seeds
+    re_seeds = re.compile(r'seeds: ([\d\s]+)')
+
+    # regexp to get all the maps titles
+    re_map_title = re.compile(r'([a-z]+-to-[a-z]+) map:')
+
+    # regexp to get all the maps
+    re_maps = re.compile(r'^(\d+) (\d+) (\d+)', re.MULTILINE)
+    for line in lines:
+        log.debug("line:%s", line.strip())
+        if seeds is None:
+            match = re_seeds.search(line)
+            if match:
+                seeds = [int(x) for x in match.group(1).split()]
+                log.debug("seeds:%s", seeds)
+                continue
+        match_title = re_map_title.search(line)
+        if match_title:
+            log.debug("-- match_title --")
+            map_title = match_title.group(1)
+            log.debug("map_title:%s", map_title)
+            if map_title not in almanac:
+                almanac[map_title] = []
+            continue
+        match_maps = re_maps.search(line)
+        if match_maps:
+            log.debug("-- match_maps --")
+            destination_range = int(match_maps.group(1))
+            source_range = int(match_maps.group(2))
+            range_length = int(match_maps.group(3))
+            log.debug("destination_range:%s source_range:%s range_length:%s",
+                      destination_range, source_range, range_length)
+            almanac[map_title].append([destination_range, source_range, range_length])
+    return seeds, almanac
+
+
+def translate_to_next(value, translators):
+    """
+    Giving a list of ranges, the one usually associated to an almanac element,
+    translate a value
+    """
+    for translator in translators:
+        stop = translator[1] + translator[2]
+        log.debug('translator:%s value:%s start:%s stop:%s', translator, value, translator[1], stop)
+        if translator and value >= translator[1] and value < stop:
+            return value + translator[0] - translator[1]
+    return value
+
+
+def next_key(key, almanac):
+    """
+    Giving an almanac and a key, 
+    search for the right almanac section title like `seed-to-something`
+    and get the key of the next section, that in this example is `something`
+    """
+    for k in almanac.keys():
+        match = re.search(rf'{key}-to-(.*)', k)
+        if match:
+            return k, match.group(1)
+    return '', ''
+
+
+def get_list_seeds(seeds):
+    """
+    New seed list decoder for the part 2
+    """
+    tot = 0
+    for idx in range(int(len(seeds)/2)):
+        start = seeds[idx*2]
+        length = seeds[1+idx*2]
+        log.debug("get_list_seeds --> staret:%s length:%s", start, length)
+        
+        # For the moment comment it as this strategy is not ok, it result in a too long list
+        # ret += [seed + start for seed in range(seeds[idx_length])]
+
+        # Just estimate how bad it is
+        tot += length      
+
+    log.debug("number of seeds for the part2 --> %s", tot)
+    return tot
+
 
 def main():
     with open(sys.argv[1], 'r', encoding='utf-8') as f:
-        score = [0, 0]
-        for line in f.readlines():
-            print(line)
-        
+        score = [None, 0]
+        seeds, almanac = decode_almanac(f.readlines())
+        log.debug("almanac:%s", almanac)
+        locations = []
+        log.info("Number of seeds for the part1 %d", len(seeds))
+        import time
+        t0 = time.time()
+        for seed in seeds:
+            current_key = 'seed'
+            current_value = seed
+            while(current_key != 'location'):
+                log.debug("START --> current_key:%s current_value:%s", current_key, current_value)
+                this_key, current_key = next_key(current_key, almanac)
+                log.debug("this_key:%s  current_key:%s", this_key, current_key)
+                current_value = translate_to_next(current_value, almanac[this_key])
+                log.debug("END --> current_key:%s current_value:%s", current_key, current_value)
+            locations.append(current_value)
+        t1 = time.time()
+        print(f"Time per seed:{(t1-t0)/len(seeds)}")
+        part2_seeds_num = get_list_seeds(seeds)
+        print(f"Estimated time for the part2 that has {part2_seeds_num} seeds --> {((t1-t0)/len(seeds))*part2_seeds_num}")
+        #for key, value in almanac.items():
+        #    print(key, len(value), sum([x[2] for x in value]), [x[0] for x in value] )
+        score[0] = min(locations)
     print("Part1:" + str(score[0]))
     print("Part2:" + str(score[1]))
 
--- 2023/5/test_main.py
+++ 2023/5/test_main.py
@@ -1,4 +1,58 @@
 import main
 
-def test_something():
-    pass
+def test_decode_almanac():
+    lines = [
+        "seeds: 79 14 55 13",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "soil-to-fertilizer map:",
+        "10 15 37"]
+    seed, almanac = main.decode_almanac(lines)
+    assert seed == [79, 14, 55, 13]
+    assert 'seed-to-soil' in almanac
+    assert 'soil-to-fertilizer' in almanac
+    assert [[50, 98, 2], [52, 50, 48]] == almanac['seed-to-soil']
+    assert [[10, 15, 37]] == almanac['soil-to-fertilizer']
+
+
+def test_seed_location_one_translation():
+    """
+    If there's no translators that match the number to translate
+    the number itself is the translated number
+    """
+    assert 42 == main.translate_to_next(42, [[1,1,1]])
+
+
+def test_seed_location_one_translation_range_one_key():
+    """
+    Only one translator that has source matching the number to translate
+    """
+    assert 50 == main.translate_to_next(42, [[50, 42, 1]])
+    assert 10 == main.translate_to_next(42, [[10, 42, 1]])
+
+
+def test_seed_location_two_translation_range():
+    """
+    translation not directly in source but in range
+    """
+    assert 50 == main.translate_to_next(42, [[1, 1, 1],[50, 42, 2]])
+
+
+def test_next_key():
+    almanac = {'cane-to-pane': []}
+    assert ('cane-to-pane', 'pane') == main.next_key('cane', almanac)
+
+
+def test_get_list_seeds_one_pair():
+    """
+    New seed list definition for the part 2
+    """
+    assert [1, 2, 3, 4, 5] == main.get_list_seeds([1, 5])
+
+
+def test_get_list_seeds_two_pair():
+    """
+    New seed list definition for the part 2
+    """
+    assert [1, 2, 3, 9, 10, 11, 12, 13] == main.get_list_seeds([1, 3, 9, 5])
