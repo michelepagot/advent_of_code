90fe0f23f0141f747c82e9f5fffaa6d8ac09fc5e
Michele <michele.pagot.dev@outlook.com>
2024-12-13 19:23:57 +0000

    part1 day8 2024: improved version
    Easyer to be turned in part2

--- 2024/8/solution1.py
+++ 2024/8/solution1.py
@@ -1,8 +1,25 @@
 import re
 import itertools
 
-def antinodes(A, B):
-    return [tuple([ (2 * B[r]) - A[r] for r in [0,1]]) , tuple([ (2 * A[r]) - B[r] for r in [0,1]])]
+def c_per(a,n):
+     if isinstance(n,int):
+         return (a[0]*n, a[1]*n)
+
+def c_diff(a,b):
+     return (a[0]-b[0], a[1]-b[1])
+
+def c_sum(a,b):
+     return (a[0]+b[0], a[1]+b[1])
+
+def antinodes(A, B, M):
+    res = []
+    V = c_diff(c_per(B,2), A)
+    if V[0] >= 0 and V[1] >= 0 and V[0] < M[0] and V[1] < M[1]:
+        res.append(V)
+    V = c_diff(c_per(A,2), B)
+    if V[0] >= 0 and V[1] >= 0 and V[0] < M[0] and V[1] < M[1]:
+        res.append(V)
+    return res
 
 with open('input', 'r') as f:
      data = f.readlines()
@@ -26,9 +43,8 @@ d_cols = len(data_2d[0])
 for p in parsed:
     print(f"-- Antenna freq ['{p}']:{parsed[p]}")
     for x in itertools.combinations(parsed[p], 2):
-        print(f"Calculate antinodes for pair {x}")
-        for a in antinodes(x[0], x[1]):
-            if a[0] >= 0 and a[1] >= 0 and a[0] < d_rows and a[1] < d_cols:
-                print(f"Adding antinode {a}")
-                res.add(a)
+        print(f"    Calculate antinodes for pair {x}")
+        for a in antinodes(x[0], x[1], (d_rows, d_cols)):
+            print(f"    Adding antinode {a}")
+            res.add(a)
 print(f"Solution1 {len(res)}")
