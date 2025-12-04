cd42ef1bceedd042ad619053d70d0361c4dcf530
Michele Pagot <michele.pagot@hotmail.it>
2025-12-05 00:54:16 +0100

    Refresh the workflow
    Add uv support, fix 2015 folder names, add Makefile in 2025.

--- 2025/1/main.py
+++ 2025/1/main.py
@@ -1,6 +1,11 @@
+import sys
 import utils
 
-with open('input.txt', 'r') as f:
-    sol = utils.solve(f.read(), 50)
-    print(f"Part1:{sol[0]}")
-    print(f"Part2:{sol[1]}")
+def main():
+    with open(sys.argv[1], 'r', encoding='utf-8') as f:
+        sol = utils.solve(f.read(), 50)
+        print(f"Part1:{sol[0]}")
+        print(f"Part2:{sol[1]}")
+
+if __name__ == "__main__":
+    main()
